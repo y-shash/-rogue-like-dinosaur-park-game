aca27d8a0344fe5282c36695ce513c9da19aafeb
Ashwin <ash.sarith@gmail.com>
2020-10-13 00:24:58 +0800

    asar0015 and yneh0001 collaborated
    Created Grass class as well as added pseudo code for future implementation.

--- Assignment 2/DinoLand/src/game/Grass.java
+++ Assignment 2/DinoLand/src/game/Grass.java
@@ -21,15 +21,54 @@ public class Grass extends Ground {
      *
      * @param displayChar character to display for this type of terrain
      */
-    Grass(char displayChar, int turnCount, double growthProbability, boolean nearBy){
+    Grass(char displayChar, int turnCount, double growthProbability){
         super(displayChar);
+        boolean nearBy = false;
+
+        // if the player is near the grass then it will allow the player harvest hay
+        boolean allowPickup = false;
 
         // if the character (is near + or on .) growthProbability == 0.02
         // NOTE: 'Tree' == +, '.' == ground
         // -- set the displayCharacter for the block from . to / representing change from ground grass
+        if (displayChar == '+'){
+            nearBy = true;
+            growthProbability = 0.02;
+
+
+        }
+
+        else if (displayChar == '.'&& displayChar == '+' ){
+            boolean nearby = true;
+
+        }
+
 
         // if the character .(ground) is near a block with another /(grass) growthProbability == 0.10
         // -- set the display char from . to /
 
-    }
+        }
+    // diagonal check,
+    // horizontal checks
+    // vertical checks
+
+
+        //for any square of dirt, if it is next to at least 2 squares of grass, then the probability of that square of dirt
+    // to grow grass is 0.1
+
+    //check the current Location of the square of dirt, the add 1 to that Location and check if == / (grass)
+    //then add 2 to the Location we obtained first and check if that square is == /
+    // then set the probability of the first Location tp 0.1
+    //
+    // [.],[.],[.],[.],[.]
+    // [.],[.],[/],[/],[.]
+    // [.],[.],[/],[/],[.]
+    // [.],[.],[.],[.],[.]
+    // [.],[.],[.],[.],[.]
+    // for i in range(1,5 )
+    // if the 2nd and 3rd block have grass block
+    // then set the probability of grass growth for block 5 to 0.1
+
+
+
 }
