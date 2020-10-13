ddfe6b81b38dd90b6ccae99575457a981b66a123
Desk_Ash <asar0015@student.monash.edu>
2020-10-13 23:00:39 +0800

    asar0015
    asar0015__made changes to the grass, hungry dinos class, further implementation later on

--- Assignment 2/DinoLand/src/game/Grass.java
+++ Assignment 2/DinoLand/src/game/Grass.java
@@ -5,10 +5,9 @@ import edu.monash.fit2099.engine.Ground;
 public class Grass extends Ground {
     // bool will focus on identifying closeby squares of grass
     private boolean nearBy;
-
     // this is the general probability of grass growing on a plot of ground
     // - use if condition to set probability 2% or 10 % based on proximity of nearby grass plots
-    private float growthProbability;
+    private double growthProbability;
 
     // allows for identifying when to call method GrassGrowth
     private int turnCount;
@@ -19,35 +18,51 @@ public class Grass extends Ground {
     /**
      * Constructor.
      *
-     * @param displayChar character to display for this type of terrain
+     * @param turnCount this is used in the if condition to test when to emplace
      */
-    Grass(char displayChar, int turnCount, double growthProbability){
+    public void setTurnCount(int newTurnCount) {
+        turnCount = newTurnCount;
+    }
+
+    public void setGrowthProbability(double newGrowthProbability) {
+        growthProbability = newGrowthProbability;
+    }
+
+    public void setNearBy(boolean newNearBy) {
+        nearBy = newNearBy;
+    }
+
+    public void setRot(boolean newRot) {
+        rot = newRot;
+    }
+
+    public int getTurnCount() {
+        return turnCount;
+    }
+
+    public double getGrowthProbability() {
+        return growthProbability;
+    }
+
+    public boolean isNearBy() {
+        return nearBy;
+    }
+
+    public boolean isRot() {
+        return rot;
+    }
+
+    Grass(char displayChar, int turnCount) {
+
         super(displayChar);
-        boolean nearBy = false;
-
-        // if the player is near the grass then it will allow the player harvest hay
-        boolean allowPickup = false;
-
-        // if the character (is near + or on .) growthProbability == 0.02
-        // NOTE: 'Tree' == +, '.' == ground
-        // -- set the displayCharacter for the block from . to / representing change from ground grass
-        if (displayChar == '+'){
-            nearBy = true;
-            growthProbability = 0.02;
-
-
+        if (displayChar == '+' || displayChar == '.') {
+            setNearBy(true);
+            setGrowthProbability(0.02);
         }
-
-        else if (displayChar == '.'&& displayChar == '+' ){
-            boolean nearby = true;
-
-        }
-
-
+    }
+}
         // if the character .(ground) is near a block with another /(grass) growthProbability == 0.10
         // -- set the display char from . to /
-
-        }
     // diagonal check,
     // horizontal checks
     // vertical checks
@@ -71,4 +86,4 @@ public class Grass extends Ground {
 
 
 
-}
+
--- Assignment 2/DinoLand/src/game/HungryDinos.java
+++ Assignment 2/DinoLand/src/game/HungryDinos.java
@@ -2,35 +2,35 @@ package game;
 
 public class HungryDinos {
 
-    //attribute to represent the current food level of Dinosaur
-    private int FoodLvl;
-
-    //represent the max level of food a stegosaur can reach
-    private static final int MAX_FOOD_LVL = 100;
-
-    //urns counter
-    private int Turns;
-
-    // If the food level gets to zero, the stegosaur becomes unconscious
-    private boolean unconsciousness;
-
-    // after 20 turns unconsciousness, the stegosaur dies
-    private boolean dead;
-
-    HungryDinos(int FoodLvl){
-
-        if (FoodLvl == 0) {
-            unconsciousness = true;
-        }
-
-        public void is_dead(){
-            //fd
-        }
-
-
-
-
-
-    }
+//    //attribute to represent the current food level of Dinosaur
+//    private int FoodLvl;
+//
+//    //represent the max level of food a stegosaur can reach
+//    private static final int MAX_FOOD_LVL = 100;
+//
+//    //turns counter
+//    private int Turns;
+//
+//    // If the food level gets to zero, the stegosaur becomes unconscious
+//    private boolean unconsciousness;
+//
+//    // after 20 turns unconsciousness, the stegosaur dies
+//    private boolean dead;
+//
+//    HungryDinos(int FoodLvl){
+//
+//        if (FoodLvl == 0) {
+//            unconsciousness = true;
+//        }
+//
+//        public void is_dead(newDead){
+//            this.dead = true;
+//        }
+//
+//
+//
+//
+//
+//    }
 
 }
