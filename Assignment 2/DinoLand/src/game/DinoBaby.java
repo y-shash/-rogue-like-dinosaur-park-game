2340e9a8940e1325873baf479c7a2d868f4336fa
Youssef Nehad <yneh0001@student.monash.edu>
2020-10-20 13:47:36 +0200

    Pair work to implement the breeding classes required

---
+++ Assignment 2/DinoLand/src/game/DinoBaby.java
@@ -0,0 +1,13 @@
+package game;
+
+public class DinoBaby extends Dinosaur{
+
+
+    public DinoBaby(String name, char displayChar, int hitPoints, int hungerLvl, Species type, Gender gender) {
+        super(name, displayChar, 10, hungerLvl, type, gender);
+    }
+
+    //create a Dinosaur object and then a method to grow from baby to adult
+
+   // public Dinosaur growToAdult(){ return dino; }
+}
---
+++ Assignment 2/DinoLand/src/game/DinoEgg.java
@@ -0,0 +1,11 @@
+package game;
+
+public class DinoEgg {
+
+    DinoBaby dinoBaby = new DinoBaby("dino",'d',10,10,Species.HERBIVORE,Gender.MALE);
+
+
+    public DinoBaby hatch(){
+        return dinoBaby;
+    }
+}
